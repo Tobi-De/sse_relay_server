bad1176d267cce0ab4925b687906526ded31dbad
Tobi DEGNON <tobidegnon@proton.me>
2023-09-15 09:31:25 +0100

    feat: package can be invoke as a cmd tool

--- src/sse_relay_server/main.py
+++ src/sse_relay_server/main.py
@@ -2,6 +2,7 @@ import json
 
 import uvicorn
 import psycopg
+import argparse
 
 from sse_starlette.sse import EventSourceResponse, ServerSentEvent
 from starlette.applications import Starlette
@@ -12,9 +13,14 @@ from starlette.routing import Route
 from .config import get_database_params, get_allowed_origins, get_debug_value
 
 
+DATABASE_PARAMS = get_database_params()
+SSE_SERVER_DEBUG = get_debug_value()
+ALLOWED_ORIGINS = get_allowed_origins()
+
+
 async def event_publisher(request: Request):
     aconnection = await psycopg.AsyncConnection.connect(
-        **get_database_params(),
+        **DATABASE_PARAMS,
         autocommit=True,
     )
     channel = request.query_params.get("channel")
@@ -39,16 +45,45 @@ routes = [Route("/", endpoint=sse)]
 middleware = [
     Middleware(
         CORSMiddleware,
-        allow_origins=get_allowed_origins(),
+        allow_origins=ALLOWED_ORIGINS,
         allow_credentials=True,
         allow_methods=["GET"],
         allow_headers=["*"],
     )
 ]
 
-app = Starlette(debug=get_debug_value(), routes=routes, middleware=middleware)
+app = Starlette(debug=SSE_SERVER_DEBUG, routes=routes, middleware=middleware)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="sse-relay-server",
+        description="Relay server for SSE events",
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="0.0.0.0",
+        help="server host",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8001,
+        help="server port",
+    )
+    parser.add_argument(
+        "--reload",
+        action="store_true",
+        help="enable auto-reload",
+        default=False,
+    )
+    parser.add_argument("--workers", type=int)
+
+    args = parser.parse_args()
+
+    uvicorn.run("sse_relay_server.main:app", **args.__dict__)
 
 
 if __name__ == "__main__":
-    # pass arguments to uvicorn.run() here
-    uvicorn.run("sse_relay_server.main:app", host="0.0.0.0", port=8001)
+    main()
