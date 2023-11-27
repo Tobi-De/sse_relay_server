987b9c0ccbe976bdce9b0ab32e10865da2940568
Tobi DEGNON <tobidegnon@proton.me>
2023-11-27 10:56:26 +0100

    http request logging not that useful

--- src/sse_relay_server/main.py
+++ src/sse_relay_server/main.py
@@ -1,20 +1,15 @@
 from __future__ import annotations
 
 import argparse
-import time
 
 import structlog
 import uvicorn
-from asgi_correlation_id import CorrelationIdMiddleware
-from asgi_correlation_id.context import correlation_id
 from sse_starlette.sse import EventSourceResponse
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
-from starlette.responses import Response
 from starlette.routing import Route
-from uvicorn.protocols.utils import get_path_with_query_string
 
 from . import listen
 from ._logging import setup_logging
@@ -28,7 +23,9 @@ relay_logger = structlog.stdlib.get_logger("sse_relay_server")
 
 
 async def sse(request: Request):
-    return EventSourceResponse(listen(request.path_params.get("channel")))
+    channel = request.path_params.get("channel")
+    relay_logger.info(f"New SSE connection to {channel}")
+    return EventSourceResponse(listen(channel))
 
 
 routes = [Route("/{channel}", endpoint=sse)]
@@ -46,49 +43,49 @@ middleware = [
 app = Starlette(debug=get_debug_value(), routes=routes, middleware=middleware)
 
 
-@app.middleware("http")
-async def logging_middleware(request: Request, call_next) -> Response:
-    structlog.contextvars.clear_contextvars()
-    # These context vars will be added to all log entries emitted during the request
-    request_id = correlation_id.get()
-    structlog.contextvars.bind_contextvars(request_id=request_id)
+# @app.middleware("http")
+# async def logging_middleware(request: Request, call_next) -> Response:
+#     structlog.contextvars.clear_contextvars()
+#     # These context vars will be added to all log entries emitted during the request
+#     request_id = correlation_id.get()
+#     structlog.contextvars.bind_contextvars(request_id=request_id)
 
-    start_time = time.perf_counter_ns()
-    # If the call_next raises an error, we still want to return our own 500 response,
-    # so we can add headers to it (process time, request ID...)
-    response = Response(status_code=500)
-    try:
-        response = await call_next(request)
-    except Exception:
-        # TODO: Validate that we don't swallow exceptions (unit test?)
-        structlog.stdlib.get_logger("sse_relay_server").exception("Uncaught exception")
-        raise
-    finally:
-        process_time = time.perf_counter_ns() - start_time
-        status_code = response.status_code
-        url = get_path_with_query_string(request.scope)
-        client_host = request.client.host
-        client_port = request.client.port
-        http_method = request.method
-        http_version = request.scope["http_version"]
-        # Recreate the Uvicorn access log format, but add all parameters as structured information
-        relay_logger.info(
-            f"""{client_host}:{client_port} - "{http_method} {url} HTTP/{http_version}" {status_code}""",
-            http={
-                "url": str(request.url),
-                "status_code": status_code,
-                "method": http_method,
-                "request_id": request_id,
-                "version": http_version,
-            },
-            network={"client": {"ip": client_host, "port": client_port}},
-            duration=process_time,
-        )
-        response.headers["X-Process-Time"] = str(process_time / 10**9)
-        return response
+#     start_time = time.perf_counter_ns()
+#     # If the call_next raises an error, we still want to return our own 500 response,
+#     # so we can add headers to it (process time, request ID...)
+#     response = Response(status_code=500)
+#     try:
+#         response = await call_next(request)
+#     except Exception:
+#         # TODO: Validate that we don't swallow exceptions (unit test?)
+#         structlog.stdlib.get_logger("sse_relay_server").exception("Uncaught exception")
+#         raise
+#     finally:
+#         process_time = time.perf_counter_ns() - start_time
+#         status_code = response.status_code
+#         url = get_path_with_query_string(request.scope)
+#         client_host = request.client.host
+#         client_port = request.client.port
+#         http_method = request.method
+#         http_version = request.scope["http_version"]
+#         # Recreate the Uvicorn access log format, but add all parameters as structured information
+#         relay_logger.info(
+#             f"""{client_host}:{client_port} - "{http_method} {url} HTTP/{http_version}" {status_code}""",
+#             http={
+#                 "url": str(request.url),
+#                 "status_code": status_code,
+#                 "method": http_method,
+#                 "request_id": request_id,
+#                 "version": http_version,
+#             },
+#             network={"client": {"ip": client_host, "port": client_port}},
+#             duration=process_time,
+#         )
+#         response.headers["X-Process-Time"] = str(process_time / 10**9)
+#         return response
 
 
-app.add_middleware(CorrelationIdMiddleware)
+# app.add_middleware(CorrelationIdMiddleware)
 
 
 def main():
